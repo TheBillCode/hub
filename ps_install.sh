46e40da13d332d2511d49443e6d07a7fabd96581
sromanski@gmail.com <sromanski@gmail.com>
2021-11-24 14:29:15 -0500

    bulma files

--- ps_install.sh
+++ ps_install.sh
@@ -7,5 +7,8 @@ sudo usermod -aG docker $(whoami)
 sudo apt install python3-pip -y
 sudo pip3 install docker-compose
 git clone https://github.com/plant-studio/hub.git
+docker network create ps-net
+mkfifo /home/pi/hub/mypipe
 (crontab -l 2>/dev/null; echo "@reboot /home/pi/hub/execpipe.sh") | crontab -
+(crontab -l 2>/dev/null; echo "@reboot /home/pi/hub/start.sh") | crontab -
 sudo reboot
