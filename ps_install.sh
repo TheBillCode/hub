e4353ed72c8e454b74c8990b2bcd712f92a33ff6
plant-studio <sromanski@gmail.com>
2021-11-28 18:40:15 -0500

    Inception again

--- ps_install.sh
+++ ps_install.sh
@@ -8,7 +8,7 @@ sudo apt install python3-pip -y
 sudo pip3 install docker-compose
 git clone https://github.com/plant-studio/hub.git
 mkfifo /home/pi/hub/mypipe
-sudo mv /home/pi/hub/ps-startup.service /etc/systemd/system/ps-startup.service
+sudo cp /home/pi/hub/ps-startup.service /etc/systemd/system/ps-startup.service
 chmod 664 /etc/systemd/system/host-ip.sh
 sudo systemctl daemon-reload
 sudo systemctl enable ps-startup.service
