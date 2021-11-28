183adbab2b7db0853aa88a6a715000bbe7d89e3e
plant-studio <sromanski@gmail.com>
2021-11-28 17:19:53 -0500

    inception

--- psconfig/app/git_pull.sh
+++ psconfig/app/git_pull.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo 'cd /home/pi' > /hostpipe
 echo 'git fetch' > /hostpipe
 echo 'git reset --hard HEAD' > /hostpipe
+echo "git merge '@{u}'" > /hostpipe
 echo 'git pull https://github.com/plant-studio/hub.git' > /hostpipe
