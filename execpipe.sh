17d9c94084e53b056fe4a9419dd608de24b78e9f
sromanski@gmail.com <sromanski@gmail.com>
2021-11-28 13:22:26 -0500

    added host IP discovery

--- execpipe.sh
+++ execpipe.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/pi/hub
-/sbin/ip route|awk '/default/ { print $9 }' > psconfig/app/host_ip
+/sbin/ip route|awk '/default/ { print $9 }' > /home/pi/hub/psconfig/app/host_ip
 while true; do eval "$(cat /home/pi/hub/mypipe)" &> /home/pi/hub/output.txt; done
